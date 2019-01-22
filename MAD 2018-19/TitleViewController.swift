827a079d55e2e13085cc8036c470fbd732965680
Arthur Lafrance <alafrance@arthurs-mbp.attlocal.net>
2019-01-21 16:16:23 -0800

    finished game config screen

---
+++ MAD 2018-19/TitleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TitleViewController.swift
+//  MAD 2018-19
+//
+//  Created by Arthur Lafrance on 1/21/19.
+//  Copyright © 2019 Homestead FBLA. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelGameStart(segue: UIStoryboardSegue) {
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
