217f91961b84694250559d09e13333f7372d1645
Arthur Lafrance <alafrance@willow.local>
2019-02-02 12:41:38 -0800

    reorganized code structure

---
+++ Trivia Trek/Misc/PlayerAuth.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerAuth.swift
+//  MAD 2018-19
+//
+//  Created by Arthur Lafrance on 2/2/19.
+//  Copyright © 2019 Homestead FBLA. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+class PlayerAuth: NSObject {
+
+    static let authenticator = PlayerAuth()
+    
+}
