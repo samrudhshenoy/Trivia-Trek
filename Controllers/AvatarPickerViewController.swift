8192663bb55a99be266a00d187f53960003755bd
samrudhshenoy <45743500+samrudhshenoy@users.noreply.github.com>
2019-02-20 16:52:22 -0800

    Add files via upload

---
+++ Controllers/AvatarPickerViewController.swift
@@ -0,0 +1,105 @@
+//
+//  AvatarPickerViewController.swift
+//  MAD 2018-19
+//
+//  Created by Arthur Lafrance on 2/16/19.
+//  Copyright © 2019 Homestead FBLA. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import SCSDKLoginKit
+import SCSDKBitmojiKit
+
+class AvatarPickerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+        
+
+    @IBOutlet var mainView: UIView!
+
+    
+    @IBOutlet weak var male: UIImageView!
+    @IBOutlet weak var photos: UIImageView!
+    @IBOutlet weak var female: UIImageView!
+    
+    @IBOutlet weak var maleButton: UIButton!
+    @IBOutlet weak var photosButton: UIButton!
+    @IBOutlet weak var femaleButton: UIButton!
+    @IBOutlet weak var cancel: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.maleButton.layer.cornerRadius = 7
+        self.photosButton.layer.cornerRadius = 7
+        self.femaleButton.layer.cornerRadius = 7
+        self.cancel.layer.cornerRadius = 7
+        self.photos.layer.cornerRadius = 30
+        
+        imagePicker.delegate = self
+        
+        self.mainView.layer.cornerRadius = 10
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+    @IBAction func cancelSelection(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "rewindToHome", sender: self)
+        
+    }
+    
+    // Male button is clicked
+    @IBAction func maleClicked(_ sender: Any) {
+        // SET THE IMAGE CALLED 'cpupicmale'
+        self.performSegue(withIdentifier: "rewindToHome", sender: self)
+    }
+    
+    // Female button is clicked
+    @IBAction func femaleClicked(_ sender: Any) {
+        // SET THE IMAGE CALLED 'cpupicfemale'
+        self.performSegue(withIdentifier: "rewindToHome", sender: self)
+    }
+    
+    @IBAction func loadImageButtonTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photosButton.contentMode = .scaleAspectFit
+            photos.image = image
+            photos.clipsToBounds = true
+            self.photos.layer.cornerRadius = 44
+            // SET THE IMAGE CALLED 'image'
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controllers/HelpIntroViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HelpIntroViewController.swift
+//  MAD 2018-19
+//
+//  Created by Arthur Lafrance on 2/10/19.
+//  Copyright © 2019 Homestead FBLA. All rights reserved.
+//
+
+import UIKit
+
+class HelpIntroViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.startButton.layer.cornerRadius = 7
+        self.backButton.layer.cornerRadius = 7
+        
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "rewindToHome", sender: self)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
