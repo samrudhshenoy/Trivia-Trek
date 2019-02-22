760beb7090f639de49f50b0f48035a4502c375fb
Arthur Lafrance <alafrance@willow.local>
2019-02-21 21:50:12 -0800

    fixed bad instruction

---
+++ Trivia Trek/Models/Score.swift
@@ -0,0 +1,13 @@
+//
+//  Score.swift
+//  Trivia Trek
+//
+//  Created by Arthur Lafrance on 2/21/19.
+//  Copyright © 2019 Homestead FBLA. All rights reserved.
+//
+
+import UIKit
+
+class Score: NSObject {
+
+}
