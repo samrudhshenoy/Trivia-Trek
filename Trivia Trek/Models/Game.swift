330161646f33af1986ef8312c3ad6aba72944cef
Arthur Lafrance <alafrance@willow.local>
2019-02-24 23:39:39 -0800

    finished high score page

--- Trivia Trek/Models/Game.swift
+++ Trivia Trek/Models/Game.swift
@@ -24,9 +24,9 @@ class Game: NSObject {
     }
     
     init(fromRecord record: CKRecord) {
-        self.date = Date()
-        self.score = 0
-        self.record = CKRecord(recordType: "Game")
+        self.date = record.creationDate!
+        self.score = record.object(forKey: "score") as! Int
+        self.record = record
         
         super.init()
     }
@@ -39,50 +39,13 @@ class Game: NSObject {
         db.save(record, completionHandler: { record, error in })
     }
     
-    func getDate() {
+    func getDateStr() -> String {
         
-        var start = self.date.description.startIndex
-        var end = self.date.description.firstIndex(of: "-")
-        var dateComponents: [String] = []
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.dateFormat = "MM/dd/yyyy"
         
-        while end != nil {
-            
-            let component = String(self.date.description[start..<end!])
-//            start = self.date.description.index(after: end!)
-//            end = self.date.description.index
-            
-        }
-        
-//        var fullStr = self.date.description
-//        var separator = fullStr.firstIndex(of: "-")
-//        let year = String(fullStr[fullStr.startIndex..<separator!])
-//
-//        fullStr = String(fullStr[separator!..<fullStr.endIndex])
-//        separator = fullStr.firstIndex(of: "-")
-//        let month =
-    }
-    
-}
-
-extension String {
-    
-    func index(of char: Character, after start: String.Index?) -> String.Index {
-        
-        var start = start
-        
-        if start == nil {
-            start = self.startIndex
-        }
-
-        // find relevant subset of string from start -> end
-        var subset = String(self[self.startIndex..<start!])
-        
-        // offset start by distance from startIndex
-        // find char in subset
-        // scale back index in subset and return it
-        
-        return self.startIndex
+        return formatter.string(from: self.date)
         
     }
-    
 }
